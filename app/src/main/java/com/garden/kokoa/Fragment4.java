bc2eca98b96c75d82eae0fb53e503d9896d99ea4
Jeongwon Kim <55073876+gardenk11181@users.noreply.github.com>
2020-03-20 01:18:34 +0900

    KBackgroundThread add

--- app/src/main/java/com/garden/kokoa/Fragment4.java
+++ app/src/main/java/com/garden/kokoa/Fragment4.java
@@ -42,46 +42,7 @@ public class Fragment4 extends Fragment {
         return rootView;
     }
 
-    class KBackgroundThread extends Thread {
-        private String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?id=";
-        private String IMG_URL = "http://openweathermap.org/img/w/";
-        private String APPID = "28f7e99b02cd5af43ef56fd473940a0d";
-        public void run() {
-            HttpURLConnection con = null ;
-            InputStream is = null;
 
-            try {
-                con = (HttpURLConnection) ( new URL(BASE_URL + "2172797" + "&appid="+APPID)).openConnection();
-                Log.d("NetWorkThread", "run: "+BASE_URL + "2172797" + "&appid="+APPID);
-                con.setRequestMethod("GET");
-                con.setDoInput(true);
-                con.setDoOutput(true);
-                con.connect();
-
-                // Let's read the response
-                StringBuffer buffer = new StringBuffer();
-                is = con.getInputStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                String line = null;
-                while (  (line = br.readLine()) != null )
-                    buffer.append(line + "\r\n");
-
-                is.close();
-                con.disconnect();
-                Log.d("NetWorkThread", "run: "+buffer.toString());
-            }
-            catch(Exception e) {
-                Log.d("NetWorkThread", "run: "+e.getMessage());
-            }
-            finally {
-                try { is.close(); } catch(Throwable t) {}
-                try { con.disconnect(); } catch(Throwable t) {}
-            }
-        }
-
-
-
-    }
 
 
 
